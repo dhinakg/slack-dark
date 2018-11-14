fba59b3405494c78cd6b50d82f1cc18aabba45bf
Dhinak G <dhinak2004@gmail.com>
2018-11-13 23:31:25 -0500

    Update loadCss.js

--- loadCss.js
+++ loadCss.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
- $.ajax({
-   url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
-   success: function(css) {
-     $("<style></style>").appendTo('head').html(css);
-     $("<style></style>").appendTo('head').html('#reply_container.upload_in_threads .inline_message_input_container {background: padding-box #545454}');
-     $("<style></style>").appendTo('head').html('.p-channel_sidebar {background: #363636 !important}');
-   }
- });
+  let tt__customCss = `.menu ul li a:not(.inline_menu_link) {color: #fff !important;}`
+  $.ajax({
+      url: 'https://cdn.rawgit.com/laCour/slack-night-mode/master/css/raw/black.css',
+      success: function(css) {
+          $("<style></style>").appendTo('head').html(css + tt__customCss);
+          $("<style></style>").appendTo('head').html('#reply_container.upload_in_threads .inline_message_input_container {background: padding-box #545454}');
+          $("<style></style>").appendTo('head').html('.p-channel_sidebar {background: #363636 !important}'); 
+          $("<style></style>").appendTo('head').html('#client_body:not(.onboarding):not(.feature_global_nav_layout):before {background: inherit;}'); 
+      }
+  });
 });
